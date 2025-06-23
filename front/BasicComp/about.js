2018a1f24e5dc076f6ea10b7290e2d13f5bed8ca
Adarsh2709 <jhaadarsh709@gmail.com>
2025-06-23 17:43:25 +0530

    Added

---
+++ front/BasicComp/about.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('About Us page loaded.');
+    // Add any specific JavaScript for the About Us page here.
+    // For example, if you had interactive elements or animations.
+});
