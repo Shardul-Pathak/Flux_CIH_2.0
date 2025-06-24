d7b9106169b6d8819545adcbcae2e508e6a8badc
Shardul-Pathak <shardulpathak796@gmail.com>
2025-06-24 08:10:40 +0530

    Flux_CIH2.0

--- src/background.js
+++
@@ -1,8 +0,0 @@
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-    if (changeInfo.status === "complete" && tab.url) {
-        const url = tab.url.toLowerCase();
-        if (url.includes("privacy") || url.includes("terms")) {
-            chrome.action.openPopup(); // works only if user triggers the extension (browser rule)
-        }
-    }
-});
